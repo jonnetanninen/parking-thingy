f7a1be421cb4f0feaca011d71a5131fdb505d2a9
Jonne Tanninen <jonnetanninen>
2021-02-05 10:55:11 +0200

    Cleanup

--- src/modules/map/components/Map.tsx
+++ src/modules/map/components/Map.tsx
@@ -10,8 +10,7 @@ import FacilityDetailsCard from './FacilityDetailsCard'
 const MAP_URL = 'https://tiles.hel.ninja/styles/hel-osm-bright/{z}/{x}/{y}.png';
 
 const Map = () => {
-  const { t, i18n } = useTranslation();
-  const currentLang = i18n.language as 'fi' | 'en' | 'sv';
+  const { t } = useTranslation();
   const [facilities, setFacilities] = useState<Facility[]>([]);
   const [activeFacility, setActiveFacility] = useState<Facility>();
 
@@ -23,43 +22,6 @@ const Map = () => {
 
     fetchFacilities();
   }, []);
-
-  /*const handleResultClick = (result) => {
-    setActiveResult(result);
-  };*/
-
-  // Centers the map to middle of all the projects on initial render
-  /*const getInitialPosition = (): [number, number] => {
-    if (searchResults.length > 0) {
-      let maxLon: number = searchResults[0].coordinate_lon;
-      let minLon: number = searchResults[0].coordinate_lon;
-      let maxLat: number = searchResults[0].coordinate_lat;
-      let minLat: number = searchResults[0].coordinate_lat;
-
-      searchResults.forEach((x) => {
-        const { coordinate_lon, coordinate_lat } = x;
-        if (maxLon < coordinate_lon) {
-          maxLon = coordinate_lon;
-        }
-        if (!minLon || minLon > coordinate_lon) {
-          minLon = coordinate_lon;
-        }
-        if (!maxLat || maxLat < coordinate_lat) {
-          maxLat = coordinate_lat;
-        }
-        if (!maxLat || maxLat < coordinate_lat) {
-          minLat = coordinate_lat;
-        }
-      });
-
-      const lon = (maxLon - minLon) / 2 + minLon;
-      const lat = (maxLat - minLat) / 2 + minLat;
-
-      return [lat, lon];
-    }
-
-    return [60.17, 24.94];
-  };*/
 
   const getPolygonPositions = (facility: Facility): LatLngExpression[] => {
     return facility.location.coordinates[0].map((x: number[]) => [x[1], x[0]]);
